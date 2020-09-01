8350664736bc5b1b9e6387021f8a0ad058fe985f
ImJiHwan1 <61612132+ImJiHwan1@users.noreply.github.com>
2020-09-01 18:27:36 +0900

    Create FormCS

---
+++ Application/VisionCS/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace VisionCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBoxMain = new System.Windows.Forms.PictureBox();
+            this.label_message = new System.Windows.Forms.Label();
+            this.ButtonYUVVision = new System.Windows.Forms.Button();
+            this.ButtonRGBVision = new System.Windows.Forms.Button();
+            this.ButtonVerMirroring = new System.Windows.Forms.Button();
+            this.ButtonHorMirroring = new System.Windows.Forms.Button();
+            this.ButtonEmbossFiltering = new System.Windows.Forms.Button();
+            this.ButtonHistogramVision = new System.Windows.Forms.Button();
+            this.ButtonLoadImage = new System.Windows.Forms.Button();
+            this.ButtonClose = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBoxMain
+            // 
+            this.PictureBoxMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PictureBoxMain.Location = new System.Drawing.Point(0, 0);
+            this.PictureBoxMain.Name = "PictureBoxMain";
+            this.PictureBoxMain.Size = new System.Drawing.Size(563, 348);
+            this.PictureBoxMain.TabIndex = 0;
+            this.PictureBoxMain.TabStop = false;
+            // 
+            // label_message
+            // 
+            this.label_message.AutoSize = true;
+            this.label_message.Location = new System.Drawing.Point(3, 354);
+            this.label_message.Name = "label_message";
+            this.label_message.Size = new System.Drawing.Size(106, 15);
+            this.label_message.TabIndex = 1;
+            this.label_message.Text = "load to Image!!";
+            // 
+            // ButtonYUVVision
+            // 
+            this.ButtonYUVVision.Location = new System.Drawing.Point(17, 422);
+            this.ButtonYUVVision.Name = "ButtonYUVVision";
+            this.ButtonYUVVision.Size = new System.Drawing.Size(255, 38);
+            this.ButtonYUVVision.TabIndex = 2;
+            this.ButtonYUVVision.Text = "YUV 평면 분할(3개파일 저장)";
+            this.ButtonYUVVision.UseVisualStyleBackColor = true;
+            this.ButtonYUVVision.Click += new System.EventHandler(this.ButtonYUVVision_Click);
+            // 
+            // ButtonRGBVision
+            // 
+            this.ButtonRGBVision.Location = new System.Drawing.Point(17, 466);
+            this.ButtonRGBVision.Name = "ButtonRGBVision";
+            this.ButtonRGBVision.Size = new System.Drawing.Size(255, 38);
+            this.ButtonRGBVision.TabIndex = 3;
+            this.ButtonRGBVision.Text = "RGB 평면 분할(3개파일 저장)";
+            this.ButtonRGBVision.UseVisualStyleBackColor = true;
+            this.ButtonRGBVision.Click += new System.EventHandler(this.ButtonRGBVision_Click);
+            // 
+            // ButtonVerMirroring
+            // 
+            this.ButtonVerMirroring.Location = new System.Drawing.Point(17, 510);
+            this.ButtonVerMirroring.Name = "ButtonVerMirroring";
+            this.ButtonVerMirroring.Size = new System.Drawing.Size(255, 38);
+            this.ButtonVerMirroring.TabIndex = 4;
+            this.ButtonVerMirroring.Text = "상하 대칭변환(1개파일 저장)";
+            this.ButtonVerMirroring.UseVisualStyleBackColor = true;
+            this.ButtonVerMirroring.Click += new System.EventHandler(this.ButtonVerMirroring_Click);
+            // 
+            // ButtonHorMirroring
+            // 
+            this.ButtonHorMirroring.Location = new System.Drawing.Point(17, 554);
+            this.ButtonHorMirroring.Name = "ButtonHorMirroring";
+            this.ButtonHorMirroring.Size = new System.Drawing.Size(255, 38);
+            this.ButtonHorMirroring.TabIndex = 5;
+            this.ButtonHorMirroring.Text = "좌우 대칭변환(1개파일 저장)";
+            this.ButtonHorMirroring.UseVisualStyleBackColor = true;
+            this.ButtonHorMirroring.Click += new System.EventHandler(this.ButtonHorMirroring_Click);
+            // 
+            // ButtonEmbossFiltering
+            // 
+            this.ButtonEmbossFiltering.Location = new System.Drawing.Point(17, 598);
+            this.ButtonEmbossFiltering.Name = "ButtonEmbossFiltering";
+            this.ButtonEmbossFiltering.Size = new System.Drawing.Size(255, 38);
+            this.ButtonEmbossFiltering.TabIndex = 6;
+            this.ButtonEmbossFiltering.Text = "엠보싱 필터링(1개파일 저장)";
+            this.ButtonEmbossFiltering.UseVisualStyleBackColor = true;
+            this.ButtonEmbossFiltering.Click += new System.EventHandler(this.ButtonEmbossFiltering_Click);
+            // 
+            // ButtonHistogramVision
+            // 
+            this.ButtonHistogramVision.Location = new System.Drawing.Point(17, 642);
+            this.ButtonHistogramVision.Name = "ButtonHistogramVision";
+            this.ButtonHistogramVision.Size = new System.Drawing.Size(255, 38);
+            this.ButtonHistogramVision.TabIndex = 7;
+            this.ButtonHistogramVision.Text = "Histogram 평활화(1개파일 저장)";
+            this.ButtonHistogramVision.UseVisualStyleBackColor = true;
+            this.ButtonHistogramVision.Click += new System.EventHandler(this.ButtonHistogramVision_Click);
+            // 
+            // ButtonLoadImage
+            // 
+            this.ButtonLoadImage.Location = new System.Drawing.Point(361, 598);
+            this.ButtonLoadImage.Name = "ButtonLoadImage";
+            this.ButtonLoadImage.Size = new System.Drawing.Size(190, 38);
+            this.ButtonLoadImage.TabIndex = 8;
+            this.ButtonLoadImage.Text = "이미지 읽어오기";
+            this.ButtonLoadImage.UseVisualStyleBackColor = true;
+            this.ButtonLoadImage.Click += new System.EventHandler(this.ButtonLoadImage_Click);
+            // 
+            // ButtonClose
+            // 
+            this.ButtonClose.Location = new System.Drawing.Point(361, 642);
+            this.ButtonClose.Name = "ButtonClose";
+            this.ButtonClose.Size = new System.Drawing.Size(190, 38);
+            this.ButtonClose.TabIndex = 9;
+            this.ButtonClose.Text = "종료";
+            this.ButtonClose.UseVisualStyleBackColor = true;
+            this.ButtonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(361, 354);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 25);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 735);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.ButtonClose);
+            this.Controls.Add(this.ButtonLoadImage);
+            this.Controls.Add(this.ButtonHistogramVision);
+            this.Controls.Add(this.ButtonEmbossFiltering);
+            this.Controls.Add(this.ButtonHorMirroring);
+            this.Controls.Add(this.ButtonVerMirroring);
+            this.Controls.Add(this.ButtonRGBVision);
+            this.Controls.Add(this.ButtonYUVVision);
+            this.Controls.Add(this.label_message);
+            this.Controls.Add(this.PictureBoxMain);
+            this.Name = "Form1";
+            this.Text = "Image Processing";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBoxMain;
+        private System.Windows.Forms.Label label_message;
+        private System.Windows.Forms.Button ButtonYUVVision;
+        private System.Windows.Forms.Button ButtonRGBVision;
+        private System.Windows.Forms.Button ButtonVerMirroring;
+        private System.Windows.Forms.Button ButtonHorMirroring;
+        private System.Windows.Forms.Button ButtonEmbossFiltering;
+        private System.Windows.Forms.Button ButtonHistogramVision;
+        private System.Windows.Forms.Button ButtonLoadImage;
+        private System.Windows.Forms.Button ButtonClose;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Application/VisionCS/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+
+namespace VisionCS
+{
+
+    public partial class Form1 : Form
+    {
+        int m_hDLL;
+        IntPtr intPtr;
+        Bitmap m_bmp;
+
+        [DllImport("kernel32.dll", EntryPoint = "LoadLibrary")]
+        private extern static int LoadLibrary(string libraryname);
+
+        [DllImport("kernel32.dll", EntryPoint = "GetProcAddress")]
+        private extern static IntPtr GetProcAddress(int hwnd, string proname);
+
+        [DllImport("kernel32.dll", EntryPoint = "FreeLibrary")]
+        private extern static bool FreeLibrary(int hModule);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate int EngineVisionYUVCS(byte[] Y, byte[] U, byte[] V, 
+                                                                byte[] col, int w, int h);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate int EngineVisionRGBCS( byte[] R, byte[] G, byte[] B,
+                                                                 byte[] col, int w, int h);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate int EngineImageProcessCS(
+                                                                        byte[] ret, 
+                                                                        byte[] col, int w, int h, int BitsPerPixel
+                                                                      );
+        
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate int EngineHistogramVisionCS(byte[] ret, byte[] gry, int w, int h);
+        public Form1()
+        {
+            unsafe
+            {
+                m_hDLL = LoadLibrary("VisionDll.dll");
+                if (m_hDLL == 0) MessageBox.Show("Error dll load!");
+            }
+            InitializeComponent();
+        }
+        
+        private void ButtonYUVVision_Click(object sender, EventArgs e)
+        {
+            if (PictureBoxMain.Image == null) return;
+            int w = m_bmp.Width;
+            int h = m_bmp.Height;
+
+            byte[] col = Get3BytePixelDataFromBitmap(m_bmp, w, h);
+
+            byte[] Y = new byte[w * h * 3];
+            byte[] U = new byte[w * h * 3];
+            byte[] V = new byte[w * h * 3];
+
+            intPtr = GetProcAddress(m_hDLL, "EngineSplitYUVchannels");
+            EngineVisionYUVCS SplitYUVchannels =
+                (EngineVisionYUVCS)Marshal.GetDelegateForFunctionPointer(intPtr,
+                typeof(EngineVisionYUVCS));
+
+            SplitYUVchannels(Y, U, V, col, w, h);
+
+            Bitmap bmpY = Get32BitBitmapFrom24BitPixelData(Y, w, h, 24);
+            Bitmap bmpU = Get32BitBitmapFrom24BitPixelData(U, w, h, 24);
+            Bitmap bmpV = Get32BitBitmapFrom24BitPixelData(V, w, h, 24);
+
+            bmpY.Save("Y.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            bmpU.Save("U.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            bmpV.Save("V.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            PictureBoxMain.Image = bmpY;
+            PictureBoxMain.SizeMode = PictureBoxSizeMode.StretchImage;
+            PictureBoxMain.Refresh();
+
+            label_message.Text = "w= " + PictureBoxMain.Image.Width + " h= " + PictureBoxMain.Image.Height;
+        }
+
+        private void ButtonRGBVision_Click(object sender, EventArgs e)
+        {
+            if (PictureBoxMain.Image == null) return;
+            int w = m_bmp.Width;
+            int h = m_bmp.Height;
+
+            byte[] col = Get3BytePixelDataFromBitmap(m_bmp, w, h);
+
+            byte[] R = new byte[w * h * 3];
+            byte[] G = new byte[w * h * 3];
+            byte[] B = new byte[w * h * 3];
+
+            intPtr = GetProcAddress(m_hDLL, "EngineSplitRGBchannels");
+            EngineVisionRGBCS SplitRGBchannels =
+                (EngineVisionRGBCS)Marshal.GetDelegateForFunctionPointer(intPtr,
+                typeof(EngineVisionRGBCS));
+
+            SplitRGBchannels(R, G, B, col, w, h);
+
+            Bitmap bmpR = Get32BitBitmapFrom24BitPixelData(R, w, h, 24);
+            Bitmap bmpG = Get32BitBitmapFrom24BitPixelData(G, w, h, 24);
+            Bitmap bmpB = Get32BitBitmapFrom24BitPixelData(B, w, h, 24);
+
+            bmpR.Save("R.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            bmpG.Save("G.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            bmpB.Save("B.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            PictureBoxMain.Image = bmpG;
+            PictureBoxMain.SizeMode = PictureBoxSizeMode.StretchImage;
+            PictureBoxMain.Refresh();
+
+            label_message.Text = "w= " + PictureBoxMain.Image.Width + " h= " + PictureBoxMain.Image.Height;
+        }
+
+        private void ButtonVerMirroring_Click(object sender, EventArgs e)
+        {
+            if (PictureBoxMain.Image == null) return;
+
+            int w = PictureBoxMain.Image.Width;
+            int h = PictureBoxMain.Image.Height;
+            Bitmap bmp = new Bitmap(PictureBoxMain.Image);
+            byte[] col = Get3BytePixelDataFromBitmap(bmp, w, h);
+
+            byte[] ret = new byte[w * h * 3];
+            intPtr = GetProcAddress(m_hDLL, "EngineVerImageMirror");
+            EngineImageProcessCS GetVerMirrorImage =
+                (EngineImageProcessCS)Marshal.GetDelegateForFunctionPointer(intPtr,
+                        typeof(EngineImageProcessCS));
+            GetVerMirrorImage(ret, col, w, h, 24);
+
+            Display3BytePixelDataToPictureBox(ret, w, h, 24);
+
+            Bitmap bmpret = Get32BitBitmapFrom24BitPixelData(ret, w, h, 24);
+
+            bmpret.Save("O.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            
+            label_message.Text = "수직방향 대칭변환 이미지 !!";
+        }
+
+        private void ButtonHorMirroring_Click(object sender, EventArgs e)
+        {
+            if (PictureBoxMain.Image == null) return;
+
+            int w = PictureBoxMain.Image.Width;
+            int h = PictureBoxMain.Image.Height;
+            Bitmap bmp = new Bitmap(PictureBoxMain.Image);
+
+            byte[] col = Get3BytePixelDataFromBitmap(bmp, w, h); //원본영상 가져오기
+
+            byte[] ret = new byte[w * h * 3]; // 변환영상 가져오기
+
+            // Dll함수 호출
+            intPtr = GetProcAddress(m_hDLL, "EngineHorImageMirror");
+            EngineImageProcessCS GetHorMirrorImage =
+                (EngineImageProcessCS)Marshal.GetDelegateForFunctionPointer(intPtr,
+                        typeof(EngineImageProcessCS));
+            GetHorMirrorImage(ret, col, w, h, 24);
+
+            Display3BytePixelDataToPictureBox(ret, w, h, 24);
+
+            Bitmap bmpret = Get32BitBitmapFrom24BitPixelData(ret, w, h, 24);
+
+            bmpret.Save("M.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            
+            label_message.Text = "수평방향 대칭변환 이미지 !!";
+        }
+
+        private void ButtonEmbossFiltering_Click(object sender, EventArgs e)
+        {
+            if (PictureBoxMain.Image == null) return;
+            // Photo Width,Height Setting
+            int w = PictureBoxMain.Image.Width;
+            int h = PictureBoxMain.Image.Height;
+            Bitmap bmp = new Bitmap(PictureBoxMain.Image);
+            byte[] col = Get3BytePixelDataFromBitmap(bmp, w, h);
+
+            byte[] ret = new byte[w * h * 3];
+
+            // EngineImageEmbossing Callback
+            intPtr = GetProcAddress(m_hDLL, "EngineImageEmbossing");
+            EngineImageProcessCS GetEmbossingImage =
+                (EngineImageProcessCS)Marshal.GetDelegateForFunctionPointer(intPtr,
+                        typeof(EngineImageProcessCS));
+            GetEmbossingImage(ret, col, w, h, 24);
+
+            Display3BytePixelDataToPictureBox(ret, w, h, 24);
+
+            Bitmap bmpret = Get32BitBitmapFrom24BitPixelData(ret, w, h, 24);
+
+            bmpret.Save("E.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+           
+            label_message.Text = "w= " + PictureBoxMain.Image.Width + " h= " + PictureBoxMain.Image.Height;
+        }
+
+        private void ButtonHistogramVision_Click(object sender, EventArgs e)
+        {
+            if (PictureBoxMain.Image == null) return;
+            int w = m_bmp.Width;
+            int h = m_bmp.Height;
+
+            byte[] col = Get3BytePixelDataFromBitmap(m_bmp, w, h);
+
+            byte[] ret = new byte[w * h * 3];
+
+            intPtr = GetProcAddress(m_hDLL, "EngineHistogramEqualization");
+            EngineHistogramVisionCS HistogramEqualization = (EngineHistogramVisionCS)Marshal.GetDelegateForFunctionPointer
+                                                                                                (intPtr, typeof(EngineHistogramVisionCS));
+            HistogramEqualization(ret, col, w, h);
+
+            Display3BytePixelDataToPictureBox(ret, w, h, 24);
+
+            Bitmap bmpret = Get32BitBitmapFrom24BitPixelData(ret, w, h, 24);
+
+            bmpret.Save("H.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            label_message.Text = "w= " + PictureBoxMain.Image.Width + " h= " + PictureBoxMain.Image.Height;
+        }
+       
+
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            FreeLibrary(m_hDLL);
+            this.Close();
+        }
+
+        private void ButtonLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog MyOpenFileDialog = new OpenFileDialog();
+            MyOpenFileDialog.ShowDialog();
+            string filePath = MyOpenFileDialog.FileName;
+
+            LoadBitmapDisplayViewScreen(filePath);
+        }
+        private void Display3BytePixelDataToPictureBox(byte[] col, int w, int h, int BitsPerPixel)
+        {
+            int x, y;
+            byte[] raw = new byte[w * h * 4];
+
+            for (y = 0; y < h; y++)
+            {
+                for (x = 0; x < w; x++)
+                {
+                   raw[(4 * w) * (y) + (4 * x) + 0] = col[(3 * w) * (y) + (3 * x) + 0];
+                   raw[(4 * w) * (y) + (4 * x) + 1] = col[(3 * w) * (y) + (3 * x) + 1];
+                   raw[(4 * w) * (y) + (4 * x) + 2] = col[(3 * w) * (y) + (3 * x) + 2];
+                }
+            }
+            Bitmap graybmp = RawTo32BitsBitmap(raw, w, h);
+
+            PictureBoxMain.Image = graybmp;
+            PictureBoxMain.SizeMode = PictureBoxSizeMode.StretchImage;
+            PictureBoxMain.Refresh();
+        }
+
+
+        private void LoadBitmapDisplayViewScreen(string filePath)
+        {
+            Image image = Image.FromFile(filePath);
+
+            PictureBoxMain.Image = image;
+            PictureBoxMain.SizeMode = PictureBoxSizeMode.StretchImage;
+            PictureBoxMain.Refresh();
+
+            label_message.Text = Path.GetFileName(filePath);
+
+            m_bmp = new Bitmap(image);
+            label_message.Text = "w= " + m_bmp.Width + " h= " + m_bmp.Height;
+        }
+
+        private byte[] Get3BytePixelDataFromBitmap(Bitmap bmp, int w, int h)
+        {
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            System.Drawing.Imaging.BitmapData bmpData =
+                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                                            bmp.PixelFormat);
+
+            IntPtr ptr = bmpData.Scan0;
+
+            int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+
+            byte[] colValue = new byte[bytes];
+
+            System.Runtime.InteropServices.Marshal.Copy(ptr, colValue, 0, bytes);
+
+            bmp.UnlockBits(bmpData);
+
+            byte[] col = new byte[w * h * 3]; // 3bytes/pixel data
+            for (int i = 0; i < w * h; i++)
+            {
+                col[i * 3 + 0] = colValue[i * 4 + 0]; //B
+                col[i * 3 + 1] = colValue[i * 4 + 1]; // G
+                col[i * 3 + 2] = colValue[i * 4 + 2]; // R
+            }
+            return col;
+        }
+
+        private Bitmap Get32BitBitmapFrom24BitPixelData(byte[] col, int w, int h, int BitsperPixel)
+        {
+            int x, y;
+            byte[] raw = new byte[w * h * 4];
+
+            for (y = 0; y < h; y++)
+            {
+                for (x = 0; x < w; x++)
+                {
+                   raw[(4 * w) * (y) + (4 * x) + 0] = col[(3 * w) * (y) + (3 * x) + 0];
+                   raw[(4 * w) * (y) + (4 * x) + 1] = col[(3 * w) * (y) + (3 * x) + 1];
+                   raw[(4 * w) * (y) + (4 * x) + 2] = col[(3 * w) * (y) + (3 * x) + 2];
+                }
+            }
+            Bitmap bmp = RawTo32BitsBitmap(raw, w, h);
+            return bmp;
+        }
+
+        private Bitmap RawTo32BitsBitmap(byte[] raw, int w, int h)
+        {
+            Bitmap res = new Bitmap(w, h, PixelFormat.Format32bppRgb);
+            BitmapData data = res.LockBits(new Rectangle(0, 0, w, h),
+                                                            ImageLockMode.ReadOnly,
+                                                             PixelFormat.Format32bppRgb);
+            IntPtr ptr = data.Scan0;
+            Marshal.Copy(raw, 0, ptr, w * h * 4);
+            res.UnlockBits(data);
+            return res;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
